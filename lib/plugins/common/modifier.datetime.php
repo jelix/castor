9e0661b8d7ff90a42802b216b693520814f83167
Laurent Jouanneau <ljouanneau@gmail.com>
2015-11-27 13:10:40 +0100

    Remove jLocale dependency from datetime plugin

--- lib/plugins/common/modifier.datetime.php
+++ lib/plugins/common/modifier.datetime.php
@@ -4,7 +4,7 @@
 * @subpackage jtpl_plugin
 * @author     Laurent Jouanneau
 * @contributor   Philippe Villiers
-* @copyright   2012 Laurent Jouanneau
+* @copyright   2012-2015 Laurent Jouanneau
 * @link        http://www.jelix.org
 * @licence    GNU Lesser General Public Licence see LICENCE file or http://www.gnu.org/licenses/lgpl.html
 */
@@ -16,13 +16,12 @@
  *
  * It uses DateTime to convert a date. It takes two optionnal arguments.
  * The first one is the format of the output date. It should be a format understood by DateTime,
- * or one of this format identifier: 'lang_date', 'lang_datetime', 'lang_time', 'lang_long_datetime'.
- * By default, it use the locale datetime format. 
+ * By default, it uses 'Y-m-d'. 
  * The second one is the format of the given date, if the date format is not understood by DateTime.
  *
  * examples :
  *  {$mydate|datetime}
- *  {$mydate|datetime:'dd/mm/YY'}
+ *  {$mydate|datetime:'d/m/Y'}
  *
  * @param string $date the date
  * @param string $format_in  the format identifier of the given date
@@ -30,7 +29,7 @@
  * @return string the converted date
  * @see jDateTime
  */
-function jtpl_modifier_common_datetime($date, $format_out = 'lang_datetime', $format_in='') {
+function jtpl_modifier_common_datetime($date, $format_out = 'Y-m-d', $format_in='') {
     if (!($date instanceof DateTime)) {
         if ($date == '' || $date == "0000/00/00") {
             return '';
@@ -46,16 +45,6 @@ function jtpl_modifier_common_datetime($date, $format_out = 'lang_datetime', $fo
         }
     }
 
-    $format = array(
-        'lang_date' =>'jelix~format.date',
-        'lang_datetime' =>'jelix~format.short_datetime',
-        'lang_time' =>'jelix~format.time',
-        'lang_long_datetime' =>'jelix~format.datetime',
-    );
-
-    if (isset($format[$format_out])) {
-        $format_out = jLocale::get($format[$format_out]);
-    }
     return $date->format($format_out);
 }
 
