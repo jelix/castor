9670696404057387bd4adfe4756066569d7de5d7
Laurent Jouanneau <ljouanneau@gmail.com>
2015-11-27 14:44:20 +0100

    Remove jLocale dependency from number_format plugin

--- lib/plugins/common/modifier.number_format.php
+++ lib/plugins/common/modifier.number_format.php
@@ -4,15 +4,15 @@
  * @package    jelix
  * @subpackage jtpl_plugin
  * @author     Julien Issler
- * @contributor Mickael Fradin
- * @copyright  2008-2010 Julien Issler, 2009 Mickael Fradin
+ * @contributor Mickael Fradin, Laurent Jouanneau
+ * @copyright  2008-2010 Julien Issler, 2009 Mickael Fradin, 2015 Laurent Jouanneau
  * @link       http://www.jelix.org
  * @licence    GNU Lesser General Public Licence see LICENCE file or http://www.gnu.org/licenses/lgpl.html
  * @since 1.1
  */
 
 /**
- * NumberFormat plugin for jTpl that wraps PHP number_format function
+ * NumberFormat plugin for Castor that wraps PHP number_format function
  *
  * @param float $number the number to format
  * @param int $decimals the number of decimals to return
@@ -21,18 +21,12 @@
  * @return string
  */
 function jtpl_modifier_common_number_format($number, $decimals=0, $dec_point=false, $thousands_sep=false){
-    if ($dec_point == false) {
-        $dec_point = jLocale::get('jelix~format.decimal_point');
+
+    if ($dec_point === false && $thousands_sep === false) {
+        $number = number_format($number, $decimals);
     }
-    if ($thousands_sep === false) {
-        $thousands_sep = jLocale::get('jelix~format.thousands_sep');
-        if(strlen($thousands_sep) > 1){
-            $real_thousands_sep = $thousands_sep;
-            $thousands_sep = '#';
-        }
+    else {
+        $number = number_format($number, $decimals, ($dec_point === false ?'.':$dec_point), ($thousands_sep === false ?',':$thousands_sep));
     }
-    $number = number_format($number, $decimals, $dec_point, $thousands_sep);
-    if(isset($real_thousands_sep))
-        return str_replace($thousands_sep, $real_thousands_sep, $number);
     return $number;
 }
